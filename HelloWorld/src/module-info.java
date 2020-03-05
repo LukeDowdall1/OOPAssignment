0ba5bbc0243d417349b5ac5703d667165bcea8b9
Luke Dowdall <luke.dowdall@yahoo.ie>
2020-03-05 10:35:36 +0000

    Revert "A"
    This reverts commit c48ba2f19ab4a85a50e5c6d0b20e0125e1b91b53.

--- HelloWorld/src/module-info.java
+++
@@ -1,8 +0,0 @@
-	public static class helloworld{
-		/*This is my first 
-		Java program
-		 */
-		public static main (String[]args){
-			System/out.println("Hello World");
-		}//end main()
-}//end class
