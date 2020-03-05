c48ba2f19ab4a85a50e5c6d0b20e0125e1b91b53
Luke Dowdall <luke.dowdall@yahoo.ie>
2020-03-05 10:33:53 +0000

    A

---
+++ HelloWorld/src/module-info.java
@@ -0,0 +1,8 @@
+	public static class helloworld{
+		/*This is my first 
+		Java program
+		 */
+		public static main (String[]args){
+			System/out.println("Hello World");
+		}//end main()
+}//end class
