037f97b4acffcf4e4c7daafa71dafe5f165e36e2
Luke Dowdall <luke.dowdall@yahoo.ie>
2020-03-28 13:15:21 +0000

    Update

---
+++ AssignmentMain/Control.java
@@ -0,0 +1,12 @@
+package AssignmentMain;
+
+public class Control 
+{
+	public static void main (String [] args)
+	{
+		@SuppressWarnings("unused")
+		Main screen = new Main("Search Engine");
+		
+	}
+
+}
